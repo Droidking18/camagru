d8b4e55ffddcd65e5e9c8a3318da60ffe4f4e426
Dovi KAPLAN <dkaplan@e5r11p5.wethinkcode.co.za>
2018-10-30 09:01:46 +0200

    Fix account change. Prepared statement now. Seems all good

--- change.php
+++ change.php
@@ -11,27 +11,12 @@ if (isset($_SESSION['login'])) {
     if (password_verify($_POST['password'], $_SESSION['passhash'])) {
              if ($_POST['type'] == "notify" && $_POST['detail'] != "Y" || $_POST['type'] == "notify" && $_POST['detail'] != "N" || $_POST['type'] != "notify") {
                  try {
-                     $sql = "UPDATE users SET " . $_POST['type'] . "='" . $_POST['detail'] .  "' WHERE login = '" . $_SESSION['login'] . "';";
-                    $conn->exec($sql);
-                }
-                catch (exception $e) {
+                     $sql = "UPDATE users SET " . $_POST['type'] . "= ?  WHERE login = ?";
+                     $statement= $conn->prepare($sql);
+                     $statement->execute([$_POST['detail'], $_SESSION['login']]);
+                 } catch (exception $e) {
                     echo $e->getMessage();
-                }
+                 }
              }
     }
-    //}
-    //catch (PDOException $e) {
-    //    echo "Error" . $e->getMessage();
-    //}
 }
-/*if (!$_GET['action'] && !$_POST['id'] && $_GET['id']) {
-   9         $conn = getDB();
-  10         $sql = "SELECT * FROM Users";
-  11         foreach ($conn->query($sql) as $user) {
-  12             if ($user['id'] == $_GET['id']) {
-  13                 $sql = "UPDATE Users SET emailverify = 'Y' WHERE id = '" . $_GET['id'] ."';";
-  14                 $conn->exec($sql);
-  15                 echo "<meta http-equiv='refresh' content='0;url=login.php' />";
-  16             }
-  17         }
-  18     }*/
